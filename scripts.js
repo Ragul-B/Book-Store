e658a6f8202657a037e19feb850c8fcf6f0310de
Ragul B <112625891+Ragul-B@users.noreply.github.com>
2024-10-04 13:26:49 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const hamburger = document.getElementById('hamburger');
 const sidebar = document.getElementById('sidebar');
 const closeBtn = document.getElementById('close-btn');
 
+
 hamburger.addEventListener('click', () => {
     sidebar.classList.add('active');
 });
